40c866494dbc5cd3b77de7a5ab5de54180cee0f3
koshal <koshal.parwan@novity.io>
2025-08-18 13:30:17 +0400

    homepage grid system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
-    <div className="">
-    
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
     </div>
   );
 }
+ 
